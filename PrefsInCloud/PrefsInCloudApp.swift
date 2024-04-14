6a87d44adec84321afe78a8fa20f4f296302e3e1
Philipp <pd95@users.noreply.github.com>
2024-04-14 12:17:06 +0200

    Initial Commit

---
+++ PrefsInCloud/PrefsInCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefsInCloudApp.swift
+//  PrefsInCloud
+//
+//  Created by Philipp on 14.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct PrefsInCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
